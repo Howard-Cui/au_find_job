f0a2ab629765abb062e7ab8099274d83fa6daca1
Howard-Cui <89081041+Howard-Cui@users.noreply.github.com>
2023-02-21 13:07:28 +1100

    first commit test

---
+++ src/main/java/com/example/aufindjob/AuFindJobApplication.java
@@ -0,0 +1,13 @@
+package com.example.aufindjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuFindJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuFindJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aufindjob/AuFindJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aufindjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuFindJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
