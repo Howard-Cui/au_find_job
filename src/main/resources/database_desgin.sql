a48001f7ba4ac4b9706985187f5c331469d205d2
Howard-Cui <89081041+Howard-Cui@users.noreply.github.com>
2023-02-21 14:03:45 +1100

    databases and muti-module project setted up.

---
+++ src/main/resources/database_desgin.sql
@@ -0,0 +1,28 @@
+drop table if exists `find_job_au`;
+
+create database if not exists `find_job_au`;
+
+use `find_job_au`;
+
+create table `position` (
+                            `id` bigint unsigned not null auto_increment,
+                            `title` varchar(100) character set utf8mb4 collate utf8mb4_unicode_ci not null comment '职位',
+                            `salary` varchar(45) character set utf8mb4 collate utf8mb4_unicode_ci not null comment '薪水',
+                            `describe` text character set utf8mb4 collate utf8mb4_unicode_ci not null comment '工作内容介绍',
+                            `base_id` bigint unsigned not null,
+                            `company` varchar(200) character set utf8mb4 collate utf8mb4_unicode_ci not null comment '工作',
+                            `img_sets` varchar(3000) character set utf8mb4 collate utf8mb4_unicode_ci not null comment '多个图片url',
+                            `tag_sets` varchar(300) character set utf8mb4 collate utf8mb4_unicode_ci not null comment '多个tags的名称',
+                            `create_time` int unsigned not null,
+                            `update_time` int unsigned not null,
+                            `is_deleted` tinyint unsigned not null default '0',
+                            primary key(`id`),
+                            index uk_base_id (`base_id`)
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci comment 	'工作岗位';
+
+create table `base` (
+                        `id` bigint unsigned not null auto_increment,
+                        `base_city` varchar(45) character set utf8mb4 collate utf8mb4_unicode_ci not null comment '城市',
+                        `base_state` varchar(3) character set utf8mb4 collate utf8mb4_unicode_ci not null comment '州',
+                        primary key(`id`)
+)engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci comment '工作地点';
