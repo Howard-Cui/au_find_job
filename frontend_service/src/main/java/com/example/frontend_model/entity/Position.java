80b1208d1d8fb08e1719a6a8cd6185c3ae02c45c
Howard-Cui <89081041+Howard-Cui@users.noreply.github.com>
2023-02-25 21:18:14 +1100

    define the DAO layers relative interfaces and classes of position database table

---
+++ frontend_service/src/main/java/com/example/frontend_model/entity/Position.java
@@ -0,0 +1,20 @@
+package com.example.frontend_model.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class Position {
+    private Long id;
+    private String title;
+    private String salary;
+    private String describe;
+    private Long baseId;
+    private String company;
+    private String imgSets;
+    private String tagSets;
+    private Integer createTime;
+    private Integer updateTime;
+    private Boolean isDeleted;
+}
